ae2863989fbbb0b0701e65abb995c81f3d497bde
Sakib_Dalal <sakibdalal73@gmail.com>
2024-01-27 17:42:11 +0530

    add language data and button functions

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import tkinter
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+# ---------LANGUAGE-DATA-FILE-SETUP
+data = pandas.read_csv("./data/Language_data.csv")
+word_list_dict = pandas.DataFrame.to_dict(data, "records")
+
+# ---------BUTTON-FUNCTION-SETUP
+
+
+def button_click():
+    new_word = random.choice(word_list_dict)
+    new_word = new_word["German"]
+    canvas.itemconfig(word_title, text=f"{new_word}")
+    canvas.itemconfig(language_title, text="German")
+
 
 # ---------UI-SETUP
 # Window setup
@@ -18,20 +34,20 @@ back_card_png = tkinter.PhotoImage(file="./images/card_back.png")
 canvas.create_image(400, 263, image=front_card_png)
 
 # Canvas text setup
-language = canvas.create_text(400, 150, text="English", font=("Ariel", 40, "italic"), fill="black")
-word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"), fill="black")
+language_title = canvas.create_text(400, 150, font=("Ariel", 40, "italic"), fill="black")
+word_title = canvas.create_text(400, 263, font=("Ariel", 60, "bold"), fill="black")
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Right Button and button image setup
 right_button_image = tkinter.PhotoImage(file="./images/right.png")
-right_button = tkinter.Button(image=right_button_image, highlightbackground=BACKGROUND_COLOR)
+right_button = tkinter.Button(image=right_button_image, highlightbackground=BACKGROUND_COLOR, command=button_click)
 right_button.grid(row=1, column=1)
 
 # Wrong Button and button image setup
 wrong_button_image = tkinter.PhotoImage(file="./images/wrong.png")
-wrong_button = tkinter.Button(image=wrong_button_image, highlightbackground=BACKGROUND_COLOR)
+wrong_button = tkinter.Button(image=wrong_button_image, highlightbackground=BACKGROUND_COLOR, command=button_click)
 wrong_button.grid(row=1, column=0)
 
+button_click()
 
 window.mainloop()
-
